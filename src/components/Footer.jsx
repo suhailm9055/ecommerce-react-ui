62acc180b7552dffe983f3e98fe2938a6b415acf
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-08 18:34:55 +0530

    footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,129 @@
-import { Facebook, Instagram, WhatsApp } from '@material-ui/icons';
-import React from 'react';
-import styled from 'styled-components';
-
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Room,
+  WhatsApp,
+} from "@material-ui/icons";
+import { Roofing } from "@mui/icons-material";
+import { ListItemAvatar } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-    margin:10px;
-    
-`
+  padding: 10px;
+  display: flex;
+`;
 const Left = styled.div`
-    
-`
+padding-left:10px;
+
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  
+  `;
+  const Desc= styled.h5`
+  margin:4px 10px;
+  width:70%;`
 const Logo = styled.h1`
-    
+margin:5px 10px;
 `
 const SocialContainer = styled.div`
-    
-`
-const SocialIcon = styled.div`
-    
-`
-const Center = styled.div`
-    
-`
-const Right = styled.div`
-    
-`
-const SocialContainer1 = styled.div`
-    
-`
 
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: ${(props) => props.color};
+  margin: 10px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Title = styled.h2``;
+const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+const ListItem = styled.li`
+  width: 50%;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+const Contactitem = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  align-items: center;
+`;
+const Payment=styled.img`
+width:35%;`
 
 const Footer = () => {
-  return(
-<>
+  return (
+    <>
+      <Container>
+        <Left>
+          <Logo>deliveRing.qa</Logo>
+          <Desc>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore itaque nobis repellendus quisquam iusto magni esse porro obcaecati placeat quos.</Desc>
+          <SocialContainer>
+            <SocialIcon color="#2c46a1">
+              <Facebook />
+            </SocialIcon>
 
-<Container>
-<Left>
-<Logo>deliveRing.qa</Logo>
-<SocialContainer>
-    <SocialIcon>
-        <Facebook/>
-    </SocialIcon>
-    <SocialIcon>
-        <Instagram/>
-    </SocialIcon>
-    <SocialIcon>
-        <WhatsApp/>
-    </SocialIcon>
-</SocialContainer>
-</Left>
-<Center>
+            <SocialIcon color="#fc050e">
+              <Instagram />
+            </SocialIcon>
 
-</Center>
-<Right>
-
-</Right>
-</Container>
-
-</>
-
-  ) 
+            <SocialIcon color="#14b446">
+              <WhatsApp />
+            </SocialIcon>
+          </SocialContainer>
+        </Left>
+        <Center>
+          <Title>Useful Links</Title>
+          <List>
+            <ListItem>Home </ListItem>
+            <ListItem>Cart </ListItem>
+            <ListItem>Boy Fashion </ListItem>
+            <ListItem>Women Fashion </ListItem>
+            <ListItem>Accessories </ListItem>
+            <ListItem>My Account </ListItem>
+            <ListItem>Order Tracking </ListItem>
+            <ListItem>Wishlist </ListItem>
+            <ListItem>Terms </ListItem>
+          </List>
+        </Center>
+        <Right>
+          <Title>Contact</Title>
+          <Contactitem>
+            <Room style={{ marginRight: "10px" }} /> Doha,Qatar
+          </Contactitem>
+          <Contactitem>
+            <Phone style={{ marginRight: "10px" }} />
+            +974 1234 5678
+          </Contactitem>
+          <Contactitem>
+            <MailOutline style={{ marginRight: "10px" }} />
+            ring@delivering.qa
+          </Contactitem>
+          <Payment src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJT4cLIx-U9ZTPsz_Z6EBE3qjHKxRW5K35-jW2sU3MhpPdaVl8WD1-55D3VJgdkR38fw&usqp=CAU"/>
+          
+        </Right>
+      </Container>
+    </>
+  );
 };
 
 export default Footer;
