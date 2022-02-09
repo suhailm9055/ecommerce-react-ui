f76f5833b073d596f42217ccfa8ae186b1c95bf7
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-09 12:42:53 +0530

    Products category page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Announcements from './components/Announcements';
 import Slider from './components/Slider';
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div>
       
-      <Home/>
+      <ProductList/>
       
     </div>
   );
