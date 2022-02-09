e0f5fc9586a9fb68415028b6ff74f541e019f69f
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-09 19:34:23 +0530

    register page done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,20 +3,70 @@ import styled from "styled-components"
 const Container=styled.div`
 width:100vw;
 height:100vh;
-background:linear-gradient(to left,#ffffffe2,#ffffff37), url("https://images.pexels.com/photos/5242808/pexels-photo-5242808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")no-repeat ;
-
-background-blend-mode: lighten;`
+background:linear-gradient(to right,#adadad97,#2c2c2cd1), url("https://images.pexels.com/photos/5242808/pexels-photo-5242808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")no-repeat ;
+display:flex;
+align-items:center;
+  justify-content:center;
+`
 const Wrapper=styled.div`
+width:35%;
+padding:20px;
+background:linear-gradient(#fffdfdb7,#fffdfd3e);
+box-shadow: 0px 0px 15px 8px #423e3eab;
+border-radius:10px;
+min-height:50%;
+
 `
 const Title=styled.h1`
+font-size:28px;
+font-weight:300;
 `
 const Form=styled.form`
+
+    display:flex;
+align-items:center;
+  justify-content:center;
+flex-wrap:wrap;
 `
 const Input=styled.input`
+flex:1;
+font-size:20px;
+min-width:40%;
+margin:20px 10px 0px 0px;
+padding:5px;
 `
 const Agreement=styled.div`
+font-size:15px;
+margin:10px 0px;
+`
+
+const ButtonContainer=styled.div`
+width:100%
+    display:flex;
+align-items:center;
+  justify-content:center;
 `
 const Button=styled.button`
+
+padding: 5px 15px;
+  padding-top: 7px;
+  font-size: 20px;
+  letter-spacing: 2px;
+  /* border-radius: 25px; */
+  font-weight: 500;
+  background: #008080;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  border: 1px solid #006363;
+  box-shadow: 2px 2px 5px 1px #11111153;
+  transition: all 0.5s ease;
+  &:hover{
+    transform:scale(1.1) ;
+    background: #06d6d6dc;
+    color: #4d4d4de6;
+    font-weight: bold;
+  }
 `
 
 const Register = () => {
@@ -34,7 +84,12 @@ const Register = () => {
             <Agreement>
                 By Creating an account, I consent to the processing of my personal data in accordance with the <b> PRIVACY POLICY</b>
             </Agreement>
+
+            <ButtonContainer>
            <Button>Create Account </Button>
+
+            </ButtonContainer>
+
         </Form>
         </Wrapper>
 
