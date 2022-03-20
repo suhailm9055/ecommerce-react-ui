ea2470cb2f769afdfa214c2c4992b9effb14b78c
suhail m <suhailm9055@gmail.com>
2022-03-20 11:49:52 +0530

    sample login added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { mobile, tablet } from "../Responsive";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -20,16 +20,15 @@ const Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   height: inherit;
-  
 `;
 
 const LogoSearchContainer = styled.div`
-display: flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   height: inherit;
   ${mobile({ flexDirection: "row-reverse" })}
-  `
+`;
 const Left = styled.div`
   flex: 1;
   display: flex;
@@ -39,43 +38,46 @@ const Language = styled.span`
   font-size: large;
   color: #818181;
   ${mobile({ display: "none" })}
-`
+`;
 const Input = styled.input`
   border: none;
   ${mobile({ width: "50px" })}
   ${tablet({ width: "40px" })}
-`
+`;
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-inline:20px;
-  display:${props => props.usercheck.user === "notLoggedIn" ? "none" : "flex"};
-`
+  margin-inline: 20px;
+  display: ${(props) =>
+    props.usercheck.user === "notLoggedIn" ? "none" : "flex"};
+`;
 const Center = styled.div`
   flex: 3;
   text-align: center;
-  display:flex;
+  display: flex;
   /* justify-content:flex-end; */
-  justify-content:${props => props.usercheck.user === "notLoggedIn" ? "center" : "flex-end"};
-  display:${props => props.display === "mobile" && "none"};
+  justify-content: ${(props) =>
+    props.usercheck.user === "notLoggedIn" ? "center" : "flex-end"};
+  display: ${(props) => props.display === "mobile" && "none"};
 
-  ${mobile({ display: props => props.display === "mobile" ? "block" : "none", justifyContent: props => props.usercheck.user === "notLoggedIn" ? "center" : "flex-end" })};
+  ${mobile({
+    display: (props) => (props.display === "mobile" ? "block" : "none"),
+    justifyContent: (props) =>
+      props.usercheck.user === "notLoggedIn" ? "center" : "flex-end",
+  })};
 `;
 const Logo = styled.h1`
-font-family: 'Noto Kufi Arabic', sans-serif;
-font-size:22px;
-font-weight:900;
-  
-`
+  font-family: "Noto Kufi Arabic", sans-serif;
+  font-size: 22px;
+  font-weight: 900;
+`;
 const LogoEng = styled.h1`
-
-font-weight: bold;
-position: relative;
-    /* top: -15px; */
-  
-`
+  font-weight: bold;
+  position: relative;
+  /* top: -15px; */
+`;
 const Right = styled.div`
   flex: 3;
   display: flex;
@@ -93,7 +95,7 @@ const IconContainer = styled.div`
   transition: all 0.1s ease;
   &:hover {
     color: #4d4b4b;
-    transform:scale(1.1)
+    transform: scale(1.1);
   }
 `;
 
@@ -106,27 +108,23 @@ const MenuItem = styled.div`
   margin-right: 20px;
   ${mobile({ marginRight: "5px" })}
   ${tablet({ marginRight: "10px" })}
-`
+`;
 const BadgeContainer = styled.div`
-  display:${props => props.usercheck.user === "notLoggedIn" ? "none" : "block"};
+  display: ${(props) =>
+    props.usercheck.user === "notLoggedIn" ? "none" : "block"};
   display: flex;
   align-items: center;
   justify-content: center;
-  
-  
-  `
+`;
 
-const Navbar =  (user ) => {
+const Navbar = (user) => {
   // const [userCart,setUserCart]= useState({currentUser})
-  const quantity = useSelector(state => state.cart.quantity);
-  const currentUser = useSelector(state => state.user?.currentUser);
- 
-  
+  const quantity = useSelector((state) => state.cart.quantity);
+  const currentUser = useSelector((state) => state.user?.currentUser);
 
   return (
     <Container>
       <Wrapper>
-
         <LogoSearchContainer>
           <Left>
             <Language>EN</Language>
@@ -136,11 +134,13 @@ const Navbar =  (user ) => {
             </SearchContainer>
           </Left>
           <Center display="mobile" usercheck={user}>
-            <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
+            <Link
+              to="/"
+              style={{ color: "inherit", textDecoration: "inherit" }}
+            >
               {/* <Logo><br/>
          التوصيل السريع | قطر </Logo> */}
-              <LogoEng>
-                e-Shop </LogoEng>
+              <LogoEng>e-Shop </LogoEng>
             </Link>
           </Center>
         </LogoSearchContainer>
@@ -148,12 +148,9 @@ const Navbar =  (user ) => {
           <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
             {/* <Logo><br/>
          التوصيل السريع | قطر </Logo> */}
-            <LogoEng>
-              e-Shop </LogoEng>
-
+            <LogoEng>e-Shop </LogoEng>
           </Link>
         </Center>
-
 
         <Right>
           {/* <Link to='/register' style={{ color: "inherit", textDecoration: "inherit" }}>
@@ -164,54 +161,53 @@ const Navbar =  (user ) => {
           <MenuItem>Sign In</MenuItem>
           </Link> */}
 
-           
-          
-
-              
-            
-        {
-          currentUser&&
-          <MenuItem>
-            <IconContainer>
-            
-            <Link to="/cart" style={{ color: "inherit", textDecoration: "inherit" ,display:"flex",alignItems:"center",justifyContent:"center"}}>
-
-
-
-
-<Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', }}>
-  {/* <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+          {currentUser && (
+            <MenuItem>
+              <IconContainer>
+                <Link
+                  to="/cart"
+                  style={{
+                    color: "inherit",
+                    textDecoration: "inherit",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    {/* <Typography sx={{ minWidth: 100 }}>Contact</Typography>
 <Typography sx={{ minWidth: 100 }}>Profile</Typography> */}
-  <Tooltip title="My Cart">
-    <IconButton
-      size="small"
-      sx={{ ml: 0, backgroundColor: "#f4ffff" }}
-      // aria-controls={open ? 'account-menu' : undefined}
-      // aria-haspopup="true"
-    // aria-expanded={open ? 'true' : undefined}
-    >
-      <BadgeContainer usercheck={user}>
-        <Badge badgeContent={quantity} color="primary">
-          <ShoppingCartOutlined sx={{ width: 30, height: 30, }} color="black" />
-        </Badge>
-      </BadgeContainer>
-
-
-
-
-    </IconButton>
-  </Tooltip>
-
-</Box>
-
-
-</Link>
-            </IconContainer>
-          </MenuItem>
-          }
+                    <Tooltip title="My Cart">
+                      <IconButton
+                        size="small"
+                        sx={{ ml: 0, backgroundColor: "#f4ffff" }}
+                        // aria-controls={open ? 'account-menu' : undefined}
+                        // aria-haspopup="true"
+                        // aria-expanded={open ? 'true' : undefined}
+                      >
+                        <BadgeContainer usercheck={user}>
+                          <Badge badgeContent={quantity} color="primary">
+                            <ShoppingCartOutlined
+                              sx={{ width: 30, height: 30 }}
+                              color="black"
+                            />
+                          </Badge>
+                        </BadgeContainer>
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                </Link>
+              </IconContainer>
+            </MenuItem>
+          )}
           <MenuItem>
             <IconContainer>
-            
               <UserDropMenu />
             </IconContainer>
           </MenuItem>
