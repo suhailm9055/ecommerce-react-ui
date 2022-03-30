2fd75fa995331030fb42091643cdd147f35e8364
suhail m <suhailm9055@gmail.com>
2022-03-30 12:26:11 +0530

    base url updates

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api"
+const BASE_URL = "https://eshop-react-node-app.herokuapp.com/api"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGE2MWI1OWIzN2Q4MGM0ZjQxMWYxMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTAxOTk0OSwiZXhwIjoxNjQ1Mjc5MTQ5fQ.0_aIY5l1d2S2U8Kx4F8wYR1c2HSgz08z8PvWEpXpx4c"
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
