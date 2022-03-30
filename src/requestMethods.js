3a59c009a4f397ffcbc41367222a6e12679eb8f9
suhail m <suhailm9055@gmail.com>
2022-03-30 12:44:43 +0530

    cors updates

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,6 +4,7 @@ const BASE_URL = "https://eshop-react-node-app.herokuapp.com/api"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGE2MWI1OWIzN2Q4MGM0ZjQxMWYxMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NTAxOTk0OSwiZXhwIjoxNjQ1Mjc5MTQ5fQ.0_aIY5l1d2S2U8Kx4F8wYR1c2HSgz08z8PvWEpXpx4c"
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
+    Access_Control_Allow_Origin:"*",
 })
 export const userRequest = axios.create({
     baseURL:BASE_URL,
